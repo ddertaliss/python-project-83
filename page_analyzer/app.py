67a8a26db9d226fe0a796621a595fdb59fc4bf46
ddertaliss <ddertaliss@gmail.com>
2024-04-16 06:24:38 +0300

    bootstrap added

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def start():
-    return 'mhe)'
+    return render_template('home.html')
 
 if __name__ == '__main__':
     start(app)
